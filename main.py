c34b2e94e31b17eb980aef512e33b8143d655826
ChengHao <chenghao.hfut@gmail.com>
2023-12-17 21:59:24 +0800

    update main.py 命令行参数

--- main.py
+++ main.py
@@ -11,35 +11,37 @@ import os
 
 if __name__ == "__main__":
 
-    # Target_language, load_files 
+    # 检查命令行参数数量
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <target_language> <audio_file_path>")
+        sys.exit(1)
+
+    # 获取命令行参数
+    Target_language = sys.argv[1]
+    audio_path = sys.argv[2]
+
     API_KEY = "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"
-    audio_path = './data/alloy.wav'
     base_name = os.path.basename(audio_path)
     file_name = os.path.splitext(base_name)[0]
 
-    if False:
-        Detected_language = "Chinese_simplified"
-        Target_language = "English"
-    else:
-        Detected_language = "English"
-        Target_language = "Chinese_simplified"
+    # 假设您已经有了检测语言的逻辑
+    # 在这里您可以根据需要调整 Detected_language 的获取方式
+    Detected_language = "English"  # 或者使用您的逻辑来确定
 
-    model="whisper-1", 
+    model = "whisper-1"
     
     # ASR
-    whisper_instance = whisper_user(api_key=API_KEY, model=model,audio_path=audio_path)
-    # Detected_language = "Chinese"
+    whisper_instance = whisper_user(api_key=API_KEY, model=model, audio_path=audio_path)
     transcript = whisper_instance.ASR()
     print(f"transcript = {transcript.text}")
     
 
     # Translator
-    Translator = Translator(API_KEY)
-    translated_text = Translator.translate_text(transcript.text, Detected_language, Target_language)
+    translator_instance = Translator(API_KEY)
+    translated_text = translator_instance.translate_text(transcript.text, Detected_language, Target_language)
     print(f"translated_text = {translated_text}")
-
 
     # TTS
     tts = TextToSpeech(API_KEY)
-    speech_file_path = Path(__file__).parent /"results" /f"{file_name}_{Target_language}.mp3"
+    speech_file_path = Path(__file__).parent / "results" / f"{file_name}_{Target_language}.mp3"
     tts.create_speech(translated_text, speech_file_path)
